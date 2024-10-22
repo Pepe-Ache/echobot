34672c35c78f5952f68c87cdff1c32cc38de4f7f
Pepe-Ache <luis@smarttie.ca>
2024-10-22 16:22:55 -0600

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "c9d36d28-741a-4db3-b69a-e0dd328441a5")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "J_68Q~xZgxbK1H1C-QPA0.NdFh4zd26-I_TM2ccI")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
